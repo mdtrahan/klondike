65f4f44a0f27a7fdede46ac7361c0d4ff2109924
mdtrahan <maceo.d.trahan@gmail.com>
2024-05-05 15:57:32 -0700

    Add files via upload

---
+++ live_demo/js/CLang.min.js
@@ -0,0 +1,31 @@
+var TEXT_PRELOADER_CONTINUE = "START";
+var TEXT_GAMEOVER  = "YOU WIN";
+var TEXT_SCORE     = "SCORE";
+var TEXT_TIME      = "TIME";
+var TEXT_ARE_YOU_SURE = "ARE YOU SURE?";
+
+var TEXT_HELP1     = "THE CARD PILES IN BOARD, CAN BE BUILD DOWN BY DECRESCENT CARD RANK WITH ALTERNATE COLORS";
+var TEXT_HELP2     = "THE GOAL IS TO BUILD UP FOUR FOUNDATION BY SUIT, FROM ACE TO KING ";
+
+var TEXT_HELP1_PAGE2 = "SCORE SYSTEM";
+var TEXT_HELP2_PAGE2 = "YOU CAN EARN POINT BY DIFFERENT ACTION:"; 
+
+var TEXT_WIN_PAGE2 =  " Points for any card you drop on Foundation for the first time";
+var TEXT_WIN2_PAGE2 =  " Points for any card you snap on Board for the first time"
+
+var TEXT_LOSE1_PAGE2 =  "Time is your enemy! You lose "; 
+var TEXT_LOSE2_PAGE2 = " Points every ";
+var TEXT_LOSE3_PAGE2 = " seconds";
+
+var TEXT_MODE_1 = "1 card showing";
+var TEXT_MODE_3 = "3 cards showing";
+var TEXT_EASY = "EASY";
+var TEXT_NORMAL = "NORMAL";
+var TEXT_TOP_MODE = "CHOSE GAME MODE";
+
+var TEXT_SHARE_IMAGE = "200x200.jpg";
+var TEXT_SHARE_TITLE = "Congratulations!";
+var TEXT_SHARE_MSG1 = "You collected <strong>";
+var TEXT_SHARE_MSG2 = " points</strong>!<br><br>Share your score with your friends!";
+var TEXT_SHARE_SHARE1 = "My score is ";
+var TEXT_SHARE_SHARE2 = " points! Can you do better";
